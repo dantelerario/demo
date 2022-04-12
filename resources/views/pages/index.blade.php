b7ce2e570db8183ffe7d974c79a5ebd2ac69e890
Dante L'Erario <dantelerario@gmail.com>
2022-04-12 23:05:47 +0200

    second commit, starting with scss

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Demo</title>
-</head>
-<body>
-    <p>Lorem ipsum dolor sit am Lorem ipsum dolor sit am</p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Demo</title>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    </head>
+    <body>
+        <header>
+            <div class="navbar">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum omnis corrupti at nemo, vel labore aliquid quas aspernatur provident doloremque illo. Hic, magnam! Dolorum molestias molestiae, laudantium culpa vero iste!
+            </div>      
+        </header>
+    </body>
 </html>
