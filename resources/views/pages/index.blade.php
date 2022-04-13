83d248013bb69370beb7fa0d04d6ba8d793d1ffd
Dante L'Erario <dantelerario@gmail.com>
2022-04-13 22:19:07 +0200

    html done

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,8 +13,241 @@
     <body>
         <header>
             <div class="navbar">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum omnis corrupti at nemo, vel labore aliquid quas aspernatur provident doloremque illo. Hic, magnam! Dolorum molestias molestiae, laudantium culpa vero iste!
-            </div>      
+                <div class="col-lg-4">
+                </div>
+                <div class="col-lg-4">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Agenda</a></li>
+                        <li><a href="/">Material</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4">
+                    <ul>
+                        <li><a href="/"><i>Yotube</i></a></li>
+                        <li><a href="/"><i>Twitter</i></a></li>
+                        <li><a href="/"><i>Facebook</i></a></li>
+                    </ul>
+                </div>
+            </div>  
+            <div>
+                <h3>Webinar 04 November, 10:00 - 14:30 CET</h3>
+                <h1>
+                    EXPERT WORKSHOP: API <br>
+                    BASED WORKER <br>
+                    MANAGEMENT AND OSH
+                </h1>
+            </div>
+            <div class="countdown-bar">
+                <div class="countdown-el">15 days</div>
+                <div class="countdown-el">12 hours</div>
+                <div class="countdown-el">45 minutes</div>
+                <div class="countdown-el">16 seconds</div>
+            </div>
+            <div class="text-square">
+                
+            </div>    
         </header>
+        <main class="main-up">
+            <div class="container">
+                <h1>Agenda</h1>
+                <p>04 November 2021 | 10:00 - 14.30 CET</p>
+                <p>Moderator</p>
+                <p>+ Consortium</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="hour">10:00-10:10 </div>
+                    <div class="info">
+                        <div class="row"><h2>Welcome and Introduction</h2></div>
+                        <div class="row"><h3>- Ioannis Anyfantis, Consortium</h3></div> 
+                        <div class="row">
+                            <h3>Ioannis Anyfantis</h3>
+                            <h4>EU-OSHA</h4>
+                            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                accusantium doloremque laudantium, totam rem aperiam, eaque
+                                ipsa quae ab illo inventore veritatis et quasi architecto beatae
+                                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                                voluptas sit aspernatur aut odit aut fugit, sed.
+                            </p> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">10:10-10:20</div>
+                    <div class="info">
+                        <div class="row"><h2>Introduction and aims of the workshop</h2></div>
+                        <div class="row"><h3>+ Name Last Name, EU-OSHA</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">10:20-10:40</div>
+                    <div class="info">
+                        <div class="row"><h2>Key note speeches</h2></div>
+                        <div class="row"><h3>+ Name Last Name, Consortium</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">10:40-11:10</div>
+                    <div class="info">
+                        <div class="row"><h6>Plenary session 1</h6></div>
+                        <div class="row"><h2>The key findings from WP1 Task1, discussion, andvoting sessions</h2></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">11:30-11:45</div>
+                    <div class="info">
+                        <div class="row"><h2>Short break</h2></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">11:45-12:15</div>
+                    <div class="info">
+                        <div class="row"><h6>Breakout room 1</h6></div>
+                        <div class="row"><h2>Assessment of the OSH challenges and opportunities associated with AI-based systems for the automation of tasks.</h2></div>
+                        <div class="row"><h3>+ Prof. Dr. Phoebe Moore, University of Leicester (UL), UKUniversity of Leicester (UL), UK</h3></div>
+                        <div class="row"><h3>+ Prof. Dr. Robert Donoghue, University of Leicester (UL), UKUniversity of Leicester (UL),UK</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">12:15-12:45</div>
+                    <div class="info"><h2>Q&A - Discussion</h2></div>
+                </div>
+                <div class="row">
+                    <div class="hour">11:45-12:15</div>
+                    <div class="info">
+                        <div class="row"><h6>Breakout room 2</h6></div>
+                        <div class="row"><h2>Assessment of the OSH challenges and opportunities associated with the state of knowledge on advanced robotics.</h2></div>
+                        <div class="row"><h3>+ Patricia Rosen, (BAuA)</h3></div>
+                        <div class="row"><h3>+ Eva Heinold, (BAuA)</h3></div>
+                        <div class="row"><h3>+ Dr. Sascha Wischniewski, (BAuA)</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">12:15-12:45</div>
+                    <div class="info"><h2>Q&A - Discussion</h2></div>
+                </div>
+                <div class="row">
+                    <div class="hour">12:45-13:45</div>
+                    <div class="info"><h2>Lunch break</h2></div>
+                </div>
+                <div class="row">
+                    <div class="hour">13:45-14:00</div>
+                    <div class="info">
+                        <div class="row"><h6>Panel</h6></div>
+                        <div class="row"><h2>Presentation of discussion results from breakout room 1</h2></div>
+                        <div class="row"><h3>+ Prof. Dr. Phoebe Moore, (UL)</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">14:00-14:15</div>
+                    <div class="info">
+                        <div class="row"><h2>Presentation of discussion results from breakout room 2</h2></div>
+                        <div class="row"><h3>+ Patricia Rosen, (BAuA)</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">14:15-14:45</div>
+                    <div class="info">
+                        <div class="row"><h2>Joint discussion of all results (round table)</h2></div>
+                        <div class="row"><h3>+ Dr. Sascha Wischniewski, (BAuA)</h3></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="hour">14:45-15:00</div>
+                    <div class="info">
+                        <div class="row"><h2>Summary and outlook</h2></div>
+                        <div class="row"><h3>EU-OSHA</h3></div>
+                    </div>
+                </div>
+                <div class="btn download-btn">DOWNLOAD PDF</div>
+            </div>
+        </main>
+        <main class="main-down">
+            <div class="container">
+                <div class="row">
+                    <h1>Material</h1>
+                    <div class="main-down-content">
+                        <div class="row">
+                            <div class="row-download-icon"><i></i></div>
+                            <div class="row-download">
+                                <h4></h4>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
+                                <div class="btn download-btn">DOWNLOAD <i></i></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="row-download-icon"><i></i></div>
+                            <div class="row-download">
+                                <h4></h4>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
+                                <div class="btn download-btn">DOWNLOAD <i></i></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="row-download-icon"><i></i></div>
+                            <div class="row-download">
+                                <h4></h4>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</p>
+                                <div class="btn download-btn">DOWNLOAD <i></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <p>For information and lorem ipsum dolor sit amet</p>
+                    <a href="/">Lorem ipsum dolor sit amet<i></i></a>
+                    <a href="/">Sed ut perspiciatis unde omnis iste<i></i></a>
+                    <a href="/">Sed ut perspiciatis unde omnis iste natus error sit voluptatem<i></i></a>
+                    <a href="/">Lorem ipsum dolor sit amet, consetetur<i></i></a>
+                    <a href="/">Sed ut perspiciatis unde omnis iste natus error sit<i></i></a>
+                </div>
+            </div>      
+        </main>
+        <footer>
+            <div class="col-lg-8">
+                <div class="row">
+                    <p>@ 2021 EU-OSHA | an agency of the European Union</p>
+                </div>
+                <div class="row">
+                    <ul>
+                        <li>
+                            <a href="/">Site map</a>
+                        </li>
+                        <li>
+                            <a href="/">Contact us</a>
+                        </li>
+                        <li>
+                            <a href="/">Accessibility</a>
+                        </li>
+                        <li>
+                            <a href="/">Data protection</a>
+                        </li>
+                        <li>
+                            <a href="/">Privacy notice</a>
+                        </li>
+                        <li>
+                            <a href="/">Cookies</a>
+                        </li>
+                        <li>
+                            <a href="/">Legal notice</a>
+                        </li>
+                        <li>
+                            <a href="/">RSS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div>
+                    <a href="/"><i></i>twitter</a>
+                    <a href="/"><i></i>Facebook</a>
+                    <a href="/"><i></i>Linkedin</a>
+                    <a href="/"><i></i>YouTube</a>
+                    <a href="/"><i></i>Twitch</a>
+                    <a href="/"><i></i>Boh</a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
